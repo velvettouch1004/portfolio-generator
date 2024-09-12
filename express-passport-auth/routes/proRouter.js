6e224a3c74d7e4dae0799b4f75311c850d7f69ad
codemon39 <jamespk0309@gmail.com>
2024-09-12 14:33:57 -0500

    Update proRouter.js

--- express-passport-auth/routes/proRouter.js
+++ express-passport-auth/routes/proRouter.js
@@ -6,7 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/protfoilo", (req, res) => {
-  res.send{"hello world!"}
-}
+  res.send("hello world!")
+})
   // Form validation
 module.exports = router;
